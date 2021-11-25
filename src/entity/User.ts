1270f77e82974dde201117705fad282ace66bc8d
Anthony Suárez <neolight1010@gmail.com>
2021-11-24 22:37:16 -0500

    Prettify.

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,5 +1,5 @@
-import {Entity, Column, PrimaryGeneratedColumn} from "typeorm";
-import {ObjectType, Field} from "type-graphql";
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { ObjectType, Field } from "type-graphql";
 
 @ObjectType()
 @Entity()
--- src/resolver/helloWorld.ts
+++ src/resolver/helloWorld.ts
@@ -1,4 +1,4 @@
-import {Resolver, Query} from "type-graphql";
+import { Resolver, Query } from "type-graphql";
 
 @Resolver()
 export class HelloWorldResolver {
