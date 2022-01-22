e226ffdaa2e605d7bfa09dd349ab45a46d3c6d7b
Anthony Suárez <neolight1010@gmail.com>
2022-01-22 18:48:35 -0500

    Add missing FieldResolver decorators.

--- src/resolver/HealthRecordResolver.ts
+++ src/resolver/HealthRecordResolver.ts
@@ -1,12 +1,14 @@
-import { Resolver, ResolverInterface, Root } from "type-graphql";
+import { FieldResolver, Mutation, Resolver, ResolverInterface, Root } from "type-graphql";
 import { HealthRecord } from "../entity/HealthRecord";
 
 @Resolver(() => HealthRecord)
 export class HealthRecordResolver implements ResolverInterface<HealthRecord> {
+    @FieldResolver()
     async _clientField(@Root() hr: HealthRecord) {
         return await hr.client;
     }
 
+    @FieldResolver()
     async _takenByField(@Root() hr: HealthRecord) {
         return await hr.takenBy;
     }
