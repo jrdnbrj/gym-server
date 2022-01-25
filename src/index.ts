bab9db5806bb247fe1f364b23d84d88b2ed5c3db
Anthony Suárez <neolight1010@gmail.com>
2022-01-25 16:17:02 -0500

    Undo last commit (cors to *)

--- src/index.ts
+++ src/index.ts
@@ -105,7 +105,7 @@ const main = async () => {
         app,
         cors: {
             credentials: true,
-            origin: "*", // "http://localhost:3000", // TODO: Client url as env variable.
+            origin: "http://localhost:3000", // TODO: Client url as env variable.
         },
     });
 
